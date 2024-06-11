e6314290db5863f85cb89562a85ffe05a69eea13
Asandei Stefan <asandei.stefanel@gmail.com>
2024-06-11 21:02:52 +0300

    Initial Commit

---
+++ Workbench/WorkbenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkbenchApp.swift
+//  Workbench
+//
+//  Created by Asandei Stefan on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkbenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
