03f577453d88595b5742c079af8c3dff3bbd51f1
Asandei Stefan <asandei.stefanel@gmail.com>
2024-06-11 21:41:22 +0300

    fix signing

--- Workbench/ContentView.swift
+++ Workbench/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+var listItems = ["Item 1", "Item 2", "Item 3", "Item 4"]
+
+struct ContentView: View
+{
+    @State var select: String? = "Item 1"
+    
+    var body: some View
+    {
+        VStack
+        {
+            NavigationView()
+            {
+                List(selection: $select)
+                {
+                    ForEach((0..<listItems.count), id: \.self)
+                    {index in
+                        NavigationLink(listItems[index])
+                        {
+                            Text(listItems[index])
+                                .padding(.vertical, 2.0)
+                        }
+                    }
+                    Spacer()
+                }
+                .frame(width:160)
+                .listStyle(SidebarListStyle())
+            
+                Text("Choose something")
+            }
+            .navigationTitle("My Title")
         }
-        .padding()
     }
 }
 
